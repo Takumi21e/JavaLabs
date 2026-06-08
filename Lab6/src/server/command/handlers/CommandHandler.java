89a7943b57f2e1efba31b03aa064f05e27bd70a8
Dmitry Churochkin <churochkinD.07@mail.ru>
2026-06-08 18:51:31 +0300

    Refactor: Update CommandHandler interface - add getName and getDescription

--- Lab6/src/server/command/handlers/CommandHandler.java
+++ Lab6/src/server/command/handlers/CommandHandler.java
@@ -3,6 +3,34 @@ package server.command.handlers;
 import common.network.Request;
 import common.network.Response;
 
+/**
+ * Интерфейс обработчика команды.
+ *
+ * Определяет структуру всех обработчиков команд:
+ * - имя команды
+ * - описание команды
+ * - выполнение команды
+ */
 public interface CommandHandler {
+    /**
+     * Получить имя команды.
+     *
+     * @return имя команды
+     */
+    String getName();
+
+    /**
+     * Получить описание команды.
+     *
+     * @return описание команды
+     */
+    String getDescription();
+
+    /**
+     * Выполнить команду.
+     *
+     * @param request запрос с параметрами команды
+     * @return ответ сервера
+     */
     Response execute(Request request);
 }
